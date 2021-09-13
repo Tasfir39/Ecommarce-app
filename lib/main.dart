6869cc60a928fe736a144a277271d1c2b41a38be
Wahid <tasfirahmed56@gmail.com>
2021-09-13 10:35:19 +0600

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Ecommarce());
+}
+
+class Ecommarce extends StatelessWidget {
+  const Ecommarce({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
